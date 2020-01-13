202344aa5b7542c669205c6173a586387e80456d
Riko-fluf <59115149+Riko-fluf@users.noreply.github.com>
2020-01-14 01:17:07 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,17 @@
+import discord
+
+from discord.ext import commands
+
+from discord.ext.commands import Bot
+
+import os
+
+
+Bot = commands.Bot(command_prefix= '!')
+
+@Bot.command()
+async def hello(ctx):
+    author = ctx.message.author
+    await ctx.send('Дарова')
+
+token = os.environ.get('Bot Token')

5d5a9a4ce3e62852f93edda08c4c10655d46ad04
Riko-fluf <59115149+Riko-fluf@users.noreply.github.com>
2020-01-14 13:02:25 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -19,10 +19,4 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
 client.run(token)

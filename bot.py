678a6d9249d8448a536245136dc353a3578e72a2
Riko-fluf <59115149+Riko-fluf@users.noreply.github.com>
2020-01-14 13:14:42 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+
+import os
+
+from discord.ext import commands
+
+from discord.ext.commands import Bot
+
+Bot = commands.Bot(command_prefix= '!')
+
+@Bot.command()
+async def hello(ctx):
+    author = ctx.message.author
+    await ctx.send('Дарова')
+
+token = os.environ.get('token')
+
+bot.run(str(token))

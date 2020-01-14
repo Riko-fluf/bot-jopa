34e6498f17b529f1675c17fc294b6ce9be839c97
Riko-fluf <59115149+Riko-fluf@users.noreply.github.com>
2020-01-14 10:27:54 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,5 +13,16 @@ async def hello(ctx):
     author = ctx.message.author
     await ctx.send('Дарова')
 
-token = os.environ.get('BOT_TOKEN')
-bot.run(token)
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+client.run(token)

8c0e4788d4eb2fc7dcb66a91d0c7d03324060ae7
Riko-fluf <59115149+Riko-fluf@users.noreply.github.com>
2020-01-14 03:21:20 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,7 @@
 import discord
 
+import os
+
 from discord.ext import commands
 
 from discord.ext.commands import Bot
@@ -11,4 +13,6 @@ async def hello(ctx):
     author = ctx.message.author
     await ctx.send('Дарова')
 
-Bot.run('NjQ0MjI2MjQ1NzcyNzA1Nzkz.Xh0G1g.O45AvW5_Ssy-dDZnXvi0vI4dd5s')
+token = os 
+
+Bot.run(str(token))
